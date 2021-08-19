12e4cb68f0e8e820e0b6c7541368be5806bb99f5
Mrityunjay Mishra <mrityunjaymishra@hotmail.com>
2021-08-19 21:27:17 +0530

    Added URL customization

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Import Modules...
 const express = require('express');
-const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const compression = require('compression');
 const path = require('path')
 const mysql = require('mysql');
 const session = require('express-session');
 const MySQLStore = require('connect-mysql')(session);
-const pug = require('pug');
 const methodOverride = require('method-override');
-const fetch = require('node-fetch');
+
+// const fetch = require('node-fetch');
 
 // SEO tool ~ prerender.io
 const prerender = require('prerender-node');
@@ -41,6 +42,8 @@ app.set('views', './Views');
 app.use(prerender.set('prerenderToken','W0VDYrHkzoSOK81LX0c4'));
 
 // set-up middlewares..
+app.use(helmet()); // Security Middleware
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'Views')));
 app.use(session({
 	secret: 'thissecretcannnotberevealed',
@@ -54,18 +57,18 @@ app.use(session({
 	},
 	store: sessionStore
 }, db))
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(methodOverride("_method"));
 
 // define Routes...
 app.get('/', (req, res) => {
 	let sess = req.session;
-	const query = `SELECT * FROM Links;`;
-	db.query(query, (err, data) => {
-		if (err) throw err;
-		res.render('index', {data: sess.data});
-	});
+	// const query = `SELECT * FROM Links;`;
+	// db.query(query, (err, data) => {
+	// 	if (err) throw err;
+	// });
+	res.render('index', { data: sess.data });
 });
 
 // app.get('/awake', (req, res) => {
@@ -79,11 +82,11 @@ app.get('/', (req, res) => {
 
 app.get('/:id', (req, res) => {
 
-	const ShortedUrlID = req.params.id;
+	const { id } = req.params;
 
-	const query = `SELECT * FROM Links WHERE ShortedUrlsID='${ShortedUrlID}' LIMIT 1;`;
+	const query = `SELECT * FROM Links WHERE ShortedUrlsID='${ id }' LIMIT 1;`;
 	
-	ShortedUrlID != 'favicon.ico' ?
+	id != 'favicon.ico' ?
 		db.query(query, (err, link) => {
 			if (err) throw err;
 			res.redirect(link[0].URL);
@@ -91,40 +94,41 @@ app.get('/:id', (req, res) => {
 	: res.redirect('/');
 });
 
+// GENERATE Shorten URL and STORE in Database... 
 app.post('/', (req, res) => {
 
-	let host = req.hostname;
-	let sess = req.session;
-	let originalURL = req.body.url;
+	let { hostname, body } = req;
+	let { url, slug } = body;
+ 
+	let id = !slug ? Math.random().toString(36).replace(/[^a-z0-9]/gi, '').substr(2, 8) :  slug; 
 
-	let ShortedUrlID = Math.random().toString(36).replace(/[^a-z0-9]/gi, '').substr(2, 10);
-
-	if (originalURL) {
-		const query = `INSERT INTO Links (URL, ShortedUrlsID) VALUES ('${originalURL}','${ShortedUrlID}');`;
-
-		// console.log(query)
+	if (url) {
+		const query = `INSERT INTO Links (URL, ShortedUrlsID, slug) VALUES ('${ url }','${ id }', '${ slug }');`;
 
 		db.query(query, (err, result) => {
 
 			if (err) {
-				res.status(500)
-				.send(
-					'<center><h1> Something went wrong on server,</h1>' +
-					'<h4> sorry for your Inconvenience!</h4>' +
-					'<h4>Do visit again later...</h4><h3><a href="/">Go Back</a></h3></center>'
+				res
+				.status(500)
+				.send(`
+					<center>
+						<h1> Something went wrong on server, </h1>
+						<h4> sorry for your Inconvenience! </h4>
+						<h4>Do visit again later... </h4>
+						<h3> <a href="/"> Go Back </a> </h3>
+					</center>
+					`
 				)
 			}
 
 			if(sess.urls){
-				req.session.urls += [{url: originalURL, id : ShortedUrlID}];
+				req.session.urls += [{ url, id }];
 			} else {
-				req.session.urls = [{url: originalURL, id : ShortedUrlID}];
+				req.session.urls = [{ url, id }];
 			}
-
-			// console.log('Not available');
-			res.render('index', { host, url: originalURL, id: ShortedUrlID });
+			
+			res.render('index', { hostname, url, id });
 		});
-
 	} else {
 		res
 		.status(301)
@@ -132,9 +136,10 @@ app.post('/', (req, res) => {
 	}
 });
 
+// DELETE URL from Database...
 app.delete('/:id', (req, res) => {
-	const ShortedUrlID = req.params.id;
-	const query = `DELETE FROM Links WHERE ShortedUrlsID = '${ShortedUrlID}';`;
+	const { id }= req.params;
+	const query = `DELETE FROM Links WHERE ShortedUrlsID = '${ id }';`;
 
 	db.query(query, (err) => {
 		if (err) throw err;
@@ -153,7 +158,9 @@ app.delete('/:id', (req, res) => {
 // },1000*60*25);
 
 // Server Init...
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on PORT: ${ PORT }`));
 
 // Error 
 process.on('uncaughtException', err => console.error(err));
+
+//  END
